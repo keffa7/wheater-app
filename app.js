5c0038c1b4237f8050f2723f9b4523f9e40d8bc0
keffa7 <52319743+keffa7@users.noreply.github.com>
2022-03-09 20:29:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,93 @@
- 
-      // Varibles - placeholders
-let temperatureDescription = document.querySelector(".temperature-description");
-let temperatureDegree = document.querySelector(".temperature-degree");
-let locationTimezone = document.querySelector(".location-timezone");
-let iconPlaceholder = document.querySelector(".icon");
+  /* Hamburger menu animation */
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menu = document.querySelectorAll(".line");
+const overlay = document.querySelector(".overlay");
 
+menu.forEach(element => {
+  hamburgerMenu.addEventListener("click", function() {
+    console.log(element);
+    element.classList.toggle("anim");
+    overlay.classList.toggle("active");
+  });
+});
+      // Varibles - placeholders
+/* let temperatureDescription = document.querySelector(".temperature-description");
+*/
+let locationTimezone = document.querySelector(".location-timezone");
+let temperatureDegree = document.querySelector(".degree");
+let iconPlaceholder = document.querySelector(".wheater-icon");
+let windPlace =document.querySelector("#wind");
+let humidityPlace = document.querySelector("#humidity");
+let pressurePlace = document.querySelector("#pressure");
+
+    // Variables - placeholders
 function getWheater() {
-  let api = "https://api.weatherapi.com/v1/current.json?key=";
+  let forecast = "https://api.weatherapi.com/v1/forecast.json?key="
   let apiKey = "5a51cd4e57c94a81b9564439220802";
   navigator.geolocation.getCurrentPosition(success, error);
  
-
 function success (position) {
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
-  let url = `${api}${apiKey}&q=${lat},${long}`;
-  console.log(url);
+  let forecastUrl = `${forecast}${apiKey}&q=${lat},${long}&days=3&aqi=yes`;
+  console.log(forecastUrl);
 
-  fetch(url)
+  fetch(forecastUrl)
   .then(response => response.json())
   .then(data => {
-    console.log(data.current.condition.icon);
-    const {temp_c, condition} = data.current;
-    const {text, icon} = data.current.condition;
+    console.log(data);
+    /* Current  */
+    const {temp_c, wind_kph, humidity, pressure_mb,} = data.current;
+    const {text, icon, code} = data.current.condition;
     const {name, region, country} = data.location;
-
-   /* Add wheater data to html */ 
-  locationTimezone.textContent = `${name}, ${region}, ${country}`;
-  temperatureDegree.textContent = `${temp_c}`;
+    console.log(text);
+   /*  
   temperatureDescription.textContent = `${text}`;
- /*  iconPlaceholder.textContent = `${icon}` */
+   */
+    locationTimezone.textContent = `${name}, ${region}, ${country}`;
+    iconPlaceholder.src = `${icon}`;
+    temperatureDegree.textContent = `${temp_c}`;
+    windPlace.textContent = `${wind_kph} kph`;
+    humidityPlace.textContent =  `${humidity} %`;
+    pressurePlace.textContent = `${pressure_mb} mb`;
 
-  /* Set Icons 
-   function setIcons(icon, iconID) {
-    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-    skycons.play();
-    return Skycons.set(iconID, skycons[currentIcon]);
-  }
+    /*  Forecast  */
+    const dayOneDegree = document.querySelector(".degree-forecast-one");
+    const dayTwoDegree = document.querySelector(".degree-forecast-two");
+    const dayThreeDegree = document.querySelector(".degree-forecast-three");
+    const {forecastday} = data.forecast;
+    const {condition, uv} = data.forecast.forecastday[0].day;
   
-  setIcons(icon,iconPlaceholder); */
+    let forecastDayOne = forecastday[0].day.avgtemp_c;
+    let forecastDayTwo = forecastday[1].day.avgtemp_c;
+    let forecastDayThree = forecastday[2].day.avgtemp_c;
+  
+   /* Add wheater data to html */ 
+   const degreeSymbol = document.querySelector(".degree-symbol");
+   dayOneDegree.textContent = Math.round(forecastDayOne) + " C°";
+   dayTwoDegree.textContent = Math.round(forecastDayTwo)  + " C°";
+   dayThreeDegree.textContent = Math.round(forecastDayThree)  + " C°";
+   /* Wheater icons */
+   const dayOneIcon = document.querySelector(".forecast-icon-one");
+   dayOneIcon.src = condition.icon;
+   const dayTwoIcon = document.querySelector(".forecast-icon-two");
+   dayTwoIcon.src = data.forecast.forecastday[1].day.condition.icon;
+   const dayThreeIcon = document.querySelector(".forecast-icon-three");
+   dayThreeIcon.src = data.forecast.forecastday[2].day.condition.icon;
+   /* Overlays  */
+   const sunrisePlaceholder = document.querySelector(".sunrise");
+   const sunsetPlaceholder = document.querySelector(".sunset");
+   const moonsetPlaceholder = document.querySelector(".moonset");
+   const moonrisePlaceholder = document.querySelector(".moonrise");
+   const uvIndex = document.querySelector(".uv-index");
 
+   const {sunrise, sunset, moonrise, moonset} = data.forecast.forecastday[0].astro;
+   
+   sunrisePlaceholder.textContent = `Sunrise: ${sunrise}`;
+   sunsetPlaceholder.textContent = `Sunset: ${sunset}`;
+   moonrisePlaceholder.textContent = `Moonrise: ${moonrise}`;
+   moonsetPlaceholder.textContent = `Moonset: ${moonset}`;
+   uvIndex.textContent = `UV index: ${uv}`;
   }); 
 };
 
@@ -48,33 +96,28 @@ function error () {
   console.log(error);
 }
 }
-
 getWheater();
 
 // Get date
 const now = new Date();
-
- 
-
-console.log(now);
-
- 
-
-let days = ["Pon", "Uto", "Sri", "Čet", "Pet", "Sub", "Ned"]
-
-let day = now.getDay() - 1;
-let date = now.getDate()
-let month = now.getMonth() + 1;
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+  "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+let day = now.getDay();
+let date = now.getDate();
+let month = now.getMonth();
 let year = now.getFullYear();
 
-console.log(day, date, month, year);
-
 let whatDay = function () {
-  return days[day]
+  return days[day];
 }
-
-let fullDate = `${whatDay()}, ${date}.${month}.${year}`;
-console.log(fullDate);
+let whatMonth = function() {
+  return monthNames[month];
+}
+let fullDate = `${whatDay()}, ${whatMonth()} ${date}`;
+console.log(day, fullDate);
 // Placeholder
 const dateHolder = document.querySelector(".date");
-dateHolder.textContent = fullDate;
+dateHolder.textContent = fullDate; 
+
+     
